cb3bfa84b68bc443af720e4b0c4a0ff4ad59b481
DonPachai <33960993+DonPachai@users.noreply.github.com>
2022-11-16 22:14:23 +0530

    Initial commit

---
+++ translation.py
@@ -0,0 +1,13 @@
+class Translation(object):
+
+      # Start text
+      START = "Hi I am a channel auto forward bot. \nMake your own bot now 👉 Source code \n\n<b>Support Channel and Groups:</b>\n @GreyMatters_Bot, @greymatters_bots_discussion"             
+
+      #About text
+      ABOUT = """
+<b>📝 Language:</b> Python 3
+<b>🧰 Framework:</b> Pyrogram
+<b>👨‍💻 Developer:</b> @GreyMatter_Owner
+<b>📢 Channel:</b> @GreyMatter_Bots
+<b>👥 Group:</b> @greymatters_bots_discussion
+<b>🌐Source Code:</b> Press Me 🥰"""
